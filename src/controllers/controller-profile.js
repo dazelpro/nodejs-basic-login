cd7245f5defd0466a25d85a8e28b552fcd9baf8b
dazelpro <dazelpro@gmail.com>
2020-10-09 19:27:33 +0700

    + get data profile

--- src/controllers/controller-profile.js
+++ src/controllers/controller-profile.js
@@ -1,8 +1,31 @@
+const config = require('../configs/database');
+
+let mysql      = require('mysql');
+let pool       = mysql.createPool(config);
+
+pool.on('error',(err)=> {
+    console.error(err);
+});
+
 module.exports ={
     profile(req,res){
-        res.render("profile",{
-            url: 'http://localhost:5050/',
-            userName: req.session.username,
-        });
+        let id = req.session.userid
+        pool.getConnection(function(err, connection) {
+            if (err) throw err;
+            connection.query(
+                `
+                SELECT * FROM table_user where user_id = '${id}';
+                `
+            , function (error, results) {
+                if(error) throw error;
+                res.render("profile",{
+                    url: 'http://localhost:5050/',
+                    userName: req.session.username,
+                    nama: results[0]['user_name'],
+                    email: results[0]['user_email']
+                });
+            });
+            connection.release();
+        })
     }
 }
