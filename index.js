70dd8bc2db3d6f35d5d0188856056be83196b4cd
dazelpro <dazelpro@gmail.com>
2020-10-10 15:28:37 +0700

    + cache control

--- index.js
+++ index.js
@@ -11,10 +11,6 @@ const loginRoutes = require('./src/routes/router-login');
 const registerRoutes = require('./src/routes/router-register');
 const appRoutes = require('./src/routes/router-app');
 
-// Configurasi library bodyParser
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
 // Configurasi library session
 app.use(session({
     resave: false,
@@ -23,11 +19,20 @@ app.use(session({
     name: 'secretName',
     cookie: {
         sameSite: true,
-        maxAge: 24*60*60*1000
+        maxAge: 60000
     },
 }))
-// Gunakan flash
+
+// Configurasi dan gunakan library
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(flash());
+
+app.use(function(req, res, next) {
+    res.setHeader('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+    res.setHeader('Pragma', 'no-cache');
+    next();
+});
 
 // Setting folder views
 app.set('views',path.join(__dirname,'src/views'));
